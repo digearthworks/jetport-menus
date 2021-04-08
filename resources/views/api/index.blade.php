7c470e0bf5fe306109659a5382be5bc1d055642c
inmanturbo <inman.turbo@gmail.com>
2021-04-08 01:38:28 -0400

    remove in-progress feature work from main branch

--- resources/views/api/index.blade.php
+++ resources/views/api/index.blade.php
@@ -1,20 +1,18 @@
 <x-app-layout>
-    <div class="max-w-7xl pt-8 mx-auto px-4 sm:px-6 lg:px-8">
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('OAuth2 Applications') }}
+            {{ __('Applications') }}
         </h2>
-    </div>
+    </x-slot>
+
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @livewire('api.client-manager')
         </div>
     </div>
+
     <x-jet-section-border />
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('API Tokens') }}
-        </h2>
-    </div>
+
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @livewire('api.api-token-manager')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,41 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        @livewireStyles
 
-    @livewireStyles
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased">
+        <x-jet-banner />
 
-    <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}" defer></script>
-</head>
-
-<body class="font-sans antialiased">
-    <x-jet-banner />
-
-    <div x-data="{ open:false, sidebarOpen: false }" class="flex overflow-x-hidden min-h-screen bg-gray-100">
-        <aside class="flex-shrink-0 w-52 flex flex-col border-r transition-all duration-300" x-show="sidebarOpen"
-            {{-- :class="{ '-ml-40': !sidebarOpen }" --}}>
-
-            <nav class="flex-1 flex flex-col bg-white border-l border-gray-100">
-                <a href="#" class="p-2">Nav Link 1</a>
-                <a href="#" class="p-2">Nav Link 2</a>
-                <a href="#" class="p-2">Nav Link 3</a>
-            </nav>
-        </aside>
-        <div class="flex-1">
+        <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
-            {{-- @include('navigation-menu') --}}
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -49,14 +36,11 @@
             <!-- Page Content -->
             <main>
                 {{ $slot }}
-
             </main>
-
         </div>
-    </div>
-    @stack('modals')
 
-    @livewireScripts
-</body>
+        @stack('modals')
 
+        @livewireScripts
+    </body>
 </html>
