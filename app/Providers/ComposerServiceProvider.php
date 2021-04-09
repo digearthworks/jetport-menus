d67759d0be06ed4650d4b3ce59ccee7dd07101ec
inmanturbo <inman.turbo@gmail.com>
2021-04-09 02:20:51 -0400

    random app logo in testing

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('*', function ($view) {
+
+            $app_logo = config('ui.logo');
+
+            if(app()->environment(['testing', 'local'])){
+
+                $app_logo = '/stock-img/' . Storage::disk('stock-img')->files()[
+                    rand(0, (count(Storage::disk('stock-img')->files()) - 1))
+                ];
+            }
+
+            $view->with('app_logo', $app_logo);
+        });
+    }
+}
--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img width="227" src="{{asset(config('ui.logo', 'stock-img/qwo_logo.png'))}}">
+    <img width="227" src="{{ isset ($app_logo) ? asset($app_logo) : asset(config('ui.logo', 'stock-img/qwo_logo.png')) }}">
 </a>
