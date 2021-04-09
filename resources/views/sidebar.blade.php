b6d8433482f4cc1241a6e9e3736f292e5d308d39
inmanturbo <inman.turbo@gmail.com>
2021-04-09 13:50:01 -0400

    fix logo in sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -2,18 +2,18 @@
 
     <div class="flex justify-between h-16 bg-white border-b border-gray-100">
         <div class="flex">
-          <div class="flex-shrink-0 flex items-center">
-                      <a href="http://127.0.0.1:8000/dashboard">
-                          <img width="50" height="50" class="mx-16"  src="http://127.0.0.1:8000/stock-img/qwo_logo.png">
-                      </a>
-          </div>
+            <div class="flex-shrink-0 flex items-center">
+                <a href="{{ route('dashboard') }}">
+                    <x-jet-application-mark class="block h-9 w-auto" />
+                </a>
+            </div>
         </div>
-        </div>
+    </div>
 
     <nav class="pt-8 flex-1 flex flex-col bg-white border-l border-gray-100">
 
-        @foreach($navItems as $navItem)
-            <a href="#" class="px-2 text-gray-600">{{$navItem}}</a>
+        @foreach ($navItems as $navItem)
+            <a href="#" class="px-2 text-gray-600">{{ $navItem }}</a>
         @endforeach
 
     </nav>
