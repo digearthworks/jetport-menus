ec6d7de054d9282323f3378a348386a131b9ae01
inmanturbo <inman.turbo@gmail.com>
2021-04-16 22:21:37 -0400

    locale, helpers, datatables

---
+++ app/Helpers/Global/GeneralHelper.php
@@ -0,0 +1,46 @@
+<?php
+
+use Carbon\Carbon;
+
+if (! function_exists('appName')) {
+    /**
+     * Helper to grab the application name.
+     *
+     * @return mixed
+     */
+    function appName()
+    {
+        return config('app.name', 'Jetport');
+    }
+}
+
+if (! function_exists('carbon')) {
+    /**
+     * Create a new Carbon instance from a time.
+     *
+     * @param $time
+     *
+     * @return Carbon
+     * @throws Exception
+     */
+    function carbon($time)
+    {
+        return new Carbon($time);
+    }
+}
+
+if (! function_exists('homeRoute')) {
+    /**
+     * Return the route to the "home" page depending on authentication/authorization status.
+     *
+     * @return string
+     */
+    function homeRoute()
+    {
+        if (auth()->check()) {
+            return 'dashboard';
+        }
+
+        return 'index';
+    }
+}
