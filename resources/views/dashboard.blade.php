3b76ab0db53f971e7abd52214b0f25d4b1f6975a
inmanturbo <inman.turbo@gmail.com>
2021-04-16 20:16:24 -0400

    bring in livewire-datatables

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,21 @@
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot>
+    </x-slot> --}}
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white shadow-xl sm:rounded-lg">
-                <x-welcome />
+                @php $columns='label,group,active,icon.title,link,created_at,updated_at' @endphp
+                <livewire:datatable
+                    model="App\Models\Menu"
+                    :include="$columns"
+                    searchable="parent.label,group,active,icon.title,link"
+                    dates="created_at,updated_at"
+                    exportable
+                />
             </div>
         </div>
     </div>
