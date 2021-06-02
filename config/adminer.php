60b8bb0bc2ea54f8004eab4bddb7a8d06e97ea94
inmanturbo <inman.turbo@gmail.com>
2021-06-02 12:58:52 -0400

    add adminer

---
+++ config/adminer.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Auto Login
+    |--------------------------------------------------------------------------
+    |
+    | Enable autologin to database
+    |
+    | ATTENTION: Please only enable autologin with authenticated protection
+    |
+    */
+    'autologin' => env('ALLOW_ADMINER_AUTOLOGIN', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Route Prefix
+    |--------------------------------------------------------------------------
+    |
+    | You may customize route prefix. (default: 'adminer')
+    |
+    */
+    'route_prefix' => 'adminer',
+];
