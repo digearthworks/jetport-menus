90fa4078f9382049864d1d9bcf81a8e9e9616b59
inmanturbo <inman.turbo@gmail.com>
2021-06-05 16:26:21 -0400

    add blink cache for icons

---
+++ app/Facades/Blink.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Facades;
+
+use App\Support\Blink as AppBlink;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static mixed|\Spatie\Blink\Blink store($name = 'default')
+ *
+ * @see Statamic\Support\Blink
+ */
+class Blink extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return AppBlink::class;
+    }
+}
