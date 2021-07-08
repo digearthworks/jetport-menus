6fda0b0f2357476cec30b341955bd4a3da665f60
inmanturbo <inmanturbo@users.noreply.github.com>
2021-07-08 19:31:30 +0000

    Fix styling

--- tests/Feature/Menu/CreateMenuActionTest.php
+++ tests/Feature/Menu/CreateMenuActionTest.php
@@ -3,12 +3,8 @@
 namespace Tests\Feature\Menu;
 
 use Database\Factories\Concerns\GetsIcons;
-use Faker\Factory;
 use Tests\TestCase;
-use Turbine\Icons\Models\Icon;
 use Turbine\Menus\Actions\CreateMenuAction;
-use Turbine\Menus\Enums\MenuTemplateEnum;
-use Turbine\Menus\Enums\MenuTypeEnum;
 use Turbine\Menus\Models\Menu;
 
 class CreateMenuActionTest extends TestCase
